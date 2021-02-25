e1f372d21c20736dccd859a9c3c825be3e367966
Caio Guilherme <57760300+CaioGui123@users.noreply.github.com>
2021-02-25 01:26:31 -0300

    :hammer_and_wrench: - Starting Server and Routes

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+app.listen(3333, () =>
+  console.log('Server is running at http://localhost:3333'),
+);
